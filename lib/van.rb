1c72babc238f44991b1c9b1a9ba7da88e6153d72
ruby4life <rubyapp.master@gmail.com>
2016-05-29 20:00:15 +0100

    Van Test: should collect broken bikes from docking station. passed

--- lib/van.rb
+++ lib/van.rb
@@ -9,4 +9,17 @@ class Van
 		self.capacity = options.fetch(:capacity, capacity)
 	end
 
+	def collect_broken_bikes_from(holder)
+		holder.broken_bikes.each do |broken_bike|
+			collect_bike(holder, broken_bike)
+		end
+	end
+
+	private
+
+	def collect_bike(holder, bike)
+		dock(bike)
+		holder.release(bike)
+	end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,10 +5,19 @@ require './lib/garage'
 describe Van do 
 
 	let(:van) { Van.new(:capacity => 50) }
+	let(:dockingstation) { DockingStation.new }
+	let(:broken_bike1) { Bike.new.break! }
 	
 
 	it "should allow setting default capacity on initialising" do
 		expect(van.capacity).to eq(50)
+	end
+
+	it "should collect broken bikes from docking station" do
+		dockingstation.dock(broken_bike1)
+		van.collect_broken_bikes_from(dockingstation)
+		expect(van.bikes).to eq [broken_bike1]
+		expect(dockingstation.broken_bikes).to eq []
 	end
 
 	end
