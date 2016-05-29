7f9ff09a4f4f08bf656eaec13da96d05859f5c2b
ruby4life <rubyapp.master@gmail.com>
2016-05-29 19:52:39 +0100

    added  Van: should allow setting default capacity on initialising

---
+++ spec/van_spec.rb
@@ -0,0 +1,14 @@
+require './lib/van'
+require './lib/bike_container'
+require './lib/garage'
+
+describe Van do 
+
+	let(:van) { Van.new(:capacity => 50) }
+	
+
+	it "should allow setting default capacity on initialising" do
+		expect(van.capacity).to eq(50)
+	end
+
+	end
